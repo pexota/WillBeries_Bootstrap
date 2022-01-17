c71ca9a813b855efd328671d8e18e16e9380f2b4
Дмитрий <96527690+pexota@users.noreply.github.com>
2022-01-17 20:55:41 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+
+    const swiper = new Swiper('.swiper', {
+        speed: 400,
+        spaceBetween: 100,
+        loop: true,
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+
+ 
